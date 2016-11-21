ab67d84802658817c747646383fa7fc524213d33
Daniel Sakuma <dsakuma@gmail.com>
2016-11-21 20:54:15 -0200

    class 4

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,17 +1,29 @@
 (ns forca.core
   (:gen-class))
 
+(declare jogo)
+
 (def total-de-vidas 6)
 
 (defn perdeu [] (print "Você perdeu"))
-(defn ganhou [] (print "Você ganhou"))
+(defn ganhou [] (print "Você ganhou!"))
 
 (defn letras-faltantes [palavra acertos]
   (remove (fn [letra] (contains? acertos (str letra))) palavra))
 
-
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos))
+)
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)
+  )
 )
 
 (defn jogo [vidas palavra acertos]
@@ -19,7 +31,7 @@
     (perdeu)
     (if (acertou-a-palavra-toda? palavra acertos)
       (ganhou)
-      (print "Chuta, amigo!")
+      (avalia-chute (le-letra!) vidas palavra acertos)
     )
   )
 )
